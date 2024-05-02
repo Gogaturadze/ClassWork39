1315860583784f2b744ba3365c8a089012904a10
Gogaturadze <gogaturadze@gmail.com>
2024-05-02 15:55:29 +0400

    ClassWork39

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ClassWork39';
+
+  //
+  numbs: number[] = [];
+
+  getRandomNumber() {
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.floor(Math.random() * 41) + 10;
+      this.numbs.push(randomNumber);
+    }
+    console.log(this.numbs);
+  }
 }
